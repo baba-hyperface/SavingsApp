98ffacfafd422655691ef87a1758c7c490017860
baba-hyperface <malyam.babakhalil@hyperface.co>
2024-11-12 17:48:57 +0530

    edited routing

--- SavingsApp/src/App.jsx
+++ SavingsApp/src/App.jsx
@@ -1,60 +1,84 @@
-import { Route, Router, Routes } from 'react-router-dom'
-import Login from './components/Login'
-import Register from './components/Register'
-import Nav from './components/Nav'
-import Home from './components/Home'
-import { Box } from '@chakra-ui/react'
-import BankDeatails from './components/BankDeatails'
-import { DashBoard } from './components/DashBoard'
-import { DeActivated } from './components/DeActivated'
-import { DeActivatedPage } from './components/DeActivatedPage'
-import { SavingPlans } from './components/SavingPlans'
-import { SavingPlanPage } from './components/SavingPlanPage'
-import { Transaction } from './components/Transaction'
-import { DonutChart } from './components/DonutChart'
-import { useEffect, useState } from 'react'
-import api from './components/api'
-import Admin from './components/Admin'
-import { AdminPot } from './components/AdminPot'
-import AdminCreateUser from './components/AdminCreateuser'
+import { Route, Router, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import { Box } from "@chakra-ui/react";
+import BankDeatails from "./components/BankDeatails";
+import { DashBoard } from "./components/DashBoard";
+import { DeActivated } from "./components/DeActivated";
+import { DeActivatedPage } from "./components/DeActivatedPage";
+import { SavingPlans } from "./components/SavingPlans";
+import { SavingPlanPage } from "./components/SavingPlanPage";
+import { Transaction } from "./components/Transaction";
+import { DonutChart } from "./components/DonutChart";
+import { useEffect, useState } from "react";
+import api from "./components/api";
+import Admin from "./components/Admin";
+import { AdminPot } from "./components/AdminPot";
+import AdminCreateUser from "./components/AdminCreateuser";
+import PrivateRoute from "./PrivateRouter";
 
 function App() {
   const [potData, setPotData] = useState([]);
-  const userIdFromLocalStorage = localStorage.getItem("userid")
-
+  const userIdFromLocalStorage = localStorage.getItem("userid");
 
   useEffect(() => {
     const fetchPotData = async () => {
-        try {
-            const res = await api.get(`/user/${userIdFromLocalStorage}/savingplan`)
-            setPotData(res.data);
-            console.log(res.data);
-        } catch (error) {
-            console.log(error);
-        }
-    }
+      try {
+        const res = await api.get(`/user/${userIdFromLocalStorage}/savingplan`);
+        setPotData(res.data);
+        console.log(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
     fetchPotData();
-}, []);
+  }, []);
 
   return (
-    <Box >
-        <Nav />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/account' element={<BankDeatails/>}/>
-          <Route path='/dashboard' element={<DashBoard/>}/>
-          <Route path='/savingplan/:id' element={<SavingPlanPage />} />
-          <Route path='/deactivated' element={<DeActivatedPage />}></Route>
-          <Route path='/history' element={<Transaction />} />
-          <Route path='/chart' element={<DonutChart savingsData={potData} />} />
-          <Route path='/admin' element={<Admin />} />
+    <Box>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/account" element={<BankDeatails />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+        <Route path="/savingplan/:id" element={<SavingPlanPage />} />
+        <Route path="/deactivated" element={<DeActivatedPage />}></Route>
+        <Route path="/history" element={<Transaction />} />
+        <Route path="/chart" element={<DonutChart savingsData={potData} />} />
+        {/* <Route path='/admin' element={<Admin />} />
           <Route path='/admin/:id' element={<AdminPot />} />
-          <Route path='createuser' element={<AdminCreateUser/>}/>
-        </Routes>
-      </Box>
-  )
+          <Route path='createuser' element={<AdminCreateUser/>}/> */}
+        <Route
+          path="/admin"
+          element={
+            <PrivateRoute>
+              <Admin />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/admin/:id"
+          element={
+            <PrivateRoute>
+              <AdminPot />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/createuser"
+          element={
+            <PrivateRoute>
+              <AdminCreateUser />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </Box>
+  );
 }
 
-export default App
+export default App;
--- SavingsApp/src/components/Admin.jsx
+++ SavingsApp/src/components/Admin.jsx
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from 'react';
 import api from './api';
 import '../styles/Admin.css';
 import { Link, useNavigate } from 'react-router-dom';
-import { Box, Button, Flex, Text } from '@chakra-ui/react';
+import { Box, Button, Flex, Input, Text } from '@chakra-ui/react';
 
 export const Admin = () => {
     const [users, setUsers] = useState([]);
     const [editUser, setEditUser] = useState(null);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [loading, setLoading] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
     const nav = useNavigate();
 
 const handleNav = (userid) => {
@@ -55,17 +56,29 @@ const handleNav = (userid) => {
             console.error("Error updating user:", error);
         }
     };
+    const filteredUsers = users.filter(user => 
+        user.email.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
     if(loading) return <p>Loading...</p>
 
     return (
         <div className="admin-container">
-            <Flex justify="space-between" align="center" mb="4">
+            <Flex  justify="space-between" align="center" verticalAlign={"center"} mb="4"py={4}>
                 <Text 
                 fontWeight={"900"}
                 as={"h1"}
                 fontSize={'28px'} 
                  >User Management</Text>
+
+                <Input
+                    placeholder="Search by email"
+                    value={searchTerm}
+                    border={"1px solid"}
+                    width={"30%"}
+                    borderRadius={"20px"}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
                 <Button 
                     as={Link} 
                     to="/createuser" 
@@ -73,9 +86,11 @@ const handleNav = (userid) => {
                     size="sm"
                     borderRadius="md"
                 >
-                    + Create User
+                    + Create
+
                 </Button>
             </Flex>
+
 
             {/* <h1>User Management</h1> */}
             <table className="user-table">
@@ -91,7 +106,7 @@ const handleNav = (userid) => {
                     </tr>
                 </thead>
                 <tbody>
-                    {users.map((user) => (
+                    {filteredUsers.map((user) => (
                         <tr key={user._id}>
                             <td>{user._id}</td>
                             <td>{user.name}</td>
